7903ef166e444bb9cda6d8c3b4444be2be94f474
Nils H <44339207+NilsHellwig@users.noreply.github.com>
2021-02-14 10:15:20 +0100

    testing implementation

--- src/com/company/Knoten.java
+++ src/com/company/Knoten.java
@@ -1,16 +1,30 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
 public class Knoten {
 
     public String label;
     public String attribute;
     public String value;
+    public ArrayList<Knoten> children;
+    public String nodeId;
 
     public Knoten(){
-        // System.out.println("Neuer Knoten");
         this.label = "";
         this.value = "";
         this.attribute = "";
+
+        /* Um bei Graphviz mehrere Knoten, die das selbe Attribut besitzen
+           auseinanderhalten zu können, wird für jeden Knoten zusätzlich noch eine
+           eindeutige Id (nodeId) (8-stellig) erstellt bei der Initialisierung*/
+        createNodeId();
+    }
+
+    public void createNodeId(){
+        this.nodeId  = UUID.randomUUID().toString().substring(0, 8);
     }
 
     public void setLabel(String label){
@@ -35,6 +49,33 @@ public class Knoten {
 
     public void setValue(String value){
         this.value = value;
+    }
+
+    public void setChildren(ArrayList<Knoten> children){
+        this.children = children;
+    }
+
+    public String predict(HashMap<String, String> example){
+        if (!label.equals("")){
+            return label;
+        }
+        for (String attributeKey: example.keySet()){
+            if (attribute.equals(attributeKey)){
+                for (Knoten child: children){
+                    if (child.value.equals(example.get(attributeKey))){
+                        example.remove(attributeKey);
+                        HashMap<String,String> reducedAttributes = example;
+                        if (!child.getLabel().equals("")){
+                            System.out.println(getAttribute()+" -----> <"+child.getLabel()+">");
+                        } else {
+                            System.out.println(getAttribute() + " ---"+child.getValue()+"--> " + child.getAttribute());
+                        }
+                        return child.predict(reducedAttributes);
+                    }
+                }
+            }
+        }
+        return "no label found error";
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,40 +1,53 @@
 package com.company;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
-import java.nio.file.Path;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        // Definiere Pfade der Datensätze
-        String training_path = "src/dataset/functionality test/train.csv";
-        String test_path = "src/dataset/test.csv";
+        // Definiere Pfad der Traingsdaten
+        String trainingDataSetPath = "src/dataset/train.csv";
 
-        /* Erstelle ArrayList mit Beispieldatenpunkten, die als Hashmap repräsentiert
-         werden. */
-        ArrayList<HashMap<String,String>> dataset = getData(training_path);
+        // Jede Hasmap repräsentiert eine Zeile im Datensatz für das Training
+        ArrayList<HashMap<String,String>> dataset = getData(trainingDataSetPath);
 
+        // Definiere, welche Attribute genutzt werden sollen, um das TargetAttribute vorauszusagen
+        List<String> attributesList = Arrays.asList( "Pclass", "Sex", "SibSp","Age","Parch","Embarked");
+        ArrayList<String> attributes = new ArrayList<>(attributesList);
+
+        // Definiere, welches Attribut vorausgesagt werden soll
+        String targetAttribute = "Survived";
+
+        // "Trainiere" den Baum
+        Knoten trainedTree = trainDecisionTree(trainingDataSetPath, targetAttribute, attributes, dataset);
+
+        // Teste den Baum anhand eines Beispiels
+        HashMap<String, String> example = new HashMap<String, String>()
+        {{
+            put("Pclass", "3");
+            put("Sex", "male");
+            put("SibSp", "1");
+            put("Age", "22");
+            put("Parch", "0");
+            put("Embarked", "S");
+        }};
+        System.out.println(trainedTree.predict(example));
+
+    }
+
+    public static Knoten trainDecisionTree(String trainingDataSetPath, String targetAttribute, ArrayList<String> attributes, ArrayList<HashMap<String,String>> dataset){
         /* Diese Hashmap schaut anhand der Trainingsdaten, welche
          Werte ein Attribut überhaupt annehmen kann z.B. das Attribut Sex hat die Werte: male/female.
          Anhand der Trainingsdaten wird dies ermittelt. */
         HashMap<String,ArrayList<String>> possibleValuesForAttributes = getPossibleValuesForAttributes(dataset);
 
-        // Definiere unsere Attribute zum Vorhersagen des targetAttribute
-        ArrayList<String> attributes = new ArrayList<String>();
-        attributes.add("outlook");
-        attributes.add("humidity");
-        attributes.add("wind");
+        // Prüfe, ob Laden der CSV-Datei funktioniert hat, durch ausgeben der ersten Zeile mit Daten
+        System.out.println("<log> check dataset: "+dataset.get(0));
 
-        // Attribut, dessen Wert vorausgesagt werden soll
-        String targetAttribute = "tennis";
-
-        // Prüfe, ob Laden der CSV-Datei funktioniert hat.
-        System.out.println("<log> check dataset: "+dataset);
-
-        // Erstelle Wurzelknoten
+        // Erstelle den Startknoten
         Knoten root = lerne(dataset,targetAttribute,attributes, possibleValuesForAttributes);
+        return root;
     }
 
     public static Knoten lerne(ArrayList<HashMap<String,String>> dataset, String targetAttribute, ArrayList<String> attributes, HashMap<String,ArrayList<String>> possibleValuesForAttributes){
@@ -96,13 +109,12 @@ public class Main {
             }
         }
 
+        // Füge die Children zu dem Parent hinzu
+        root.setChildren(children);
+
         // Gebe neue Verbindungen aus
         for(Knoten child: children){
-            System.out.println("["+root.getAttribute()+" "+root.getValue()+" "+root.getLabel()+"] --> ["+child.getAttribute()+" "+child.getValue()+" "+child.getLabel()+"]");
-            if (!root.getAttribute().equals("") && !child.getAttribute().equals("")) {
-                System.out.println(root.getAttribute() + "? -> " + child.getAttribute()+"?");
-            }
-
+            System.out.println("[ <Question: "+root.getAttribute()+"> <Value of Edge: "+root.getValue()+"> <Label: "+root.getLabel()+"> ] --> [ <Question: "+child.getAttribute()+"> < Value of Edge: "+child.getValue()+"> < Label: "+child.getLabel()+"> ]");
         }
 
         return root;
@@ -130,7 +142,6 @@ public class Main {
         String mostFrequentValueForAttribute = "";
         int mostFrequentValueCount = 0;
         for (String attributeKey: valueCounter.keySet()){
-            //System.out.println(attributeKey+" "+valueCounter.get(attributeKey));
             if (valueCounter.get(attributeKey) > mostFrequentValueCount){
                 mostFrequentValueForAttribute = attributeKey;
                 mostFrequentValueCount = valueCounter.get(attributeKey);
@@ -161,26 +172,22 @@ public class Main {
                             filteredDataset.add(datapoint);
                         }
                     }
-                    //System.out.println(attribut+"------");
-                    //System.out.println(getEntropy(filteredDataset,attribut,moeglicheWerteFürZielLabel));
-                    //System.out.println("targetAtt "+attribute+" "+value+" "+getEntropy(filteredDataset,targetAttribute,possibleValuesForTargetAttribute));
                     double ev_e = (countOccuranceOfValueForAttribute / dataset.size())*getEntropy(filteredDataset,targetAttribute,possibleValuesForTargetAttribute);
                     sum+=ev_e;
                 }
-                //System.out.println(attribute);
-                //System.out.println(entropie_e+" "+sum);
                 double informationGain = entropie_e-sum;
                 informationGains.put(attribute, informationGain);
         }
         double lowest_Entropie = 0;
         for(String attributeName: informationGains.keySet()){
-            // System.out.println("Gain for "+attributeName+" "+informationGains.get(attributeName));
             if (informationGains.get(attributeName) > lowest_Entropie){
                 lowest_Entropie = informationGains.get(attributeName);
                 bestAttribute = attributeName;
             }
+            if (lowest_Entropie == 0){
+                bestAttribute = attributeName;
+            }
         }
-        // System.out.println("<log> best Attribute is "+bestAttribute);
         return bestAttribute;
     }
 
@@ -205,8 +212,7 @@ public class Main {
     }
 
     public static double log2(double N){
-        double result = Math.log(N) / Math.log(2.0);
-        return result;
+        return Math.log(N) / Math.log(2.0);
     }
 
     public static HashMap<String,ArrayList<String>> getPossibleValuesForAttributes(ArrayList<HashMap<String,String>> dataset){
@@ -215,7 +221,6 @@ public class Main {
         for(int k  = 0; k < attributes.length; k++){
             ArrayList<String> possibleValuesForAttribute = new ArrayList<>();
             for (HashMap<String,String> data : dataset){
-                /* regex would be possible too: data.get(attributes[k]).matches("[a-zA-Z0-9]+") &&*/
                 if (data.get(attributes[k]).length() > 0){
                     possibleValuesForAttribute.add(data.get(attributes[k]));
                 }
@@ -235,7 +240,9 @@ public class Main {
             int i = 0;
             String[] attributes = new String[13];
             while((line = br.readLine()) != null) {
-                // Source for regexp: https://stackoverflow.com/questions/1757065/java-splitting-a-comma-separated-string-but-ignoring-commas-in-quotes
+                /* Quelle von diesem regulären Ausdruck: https://stackoverflow.com/questions/1757065/java-splitting-a-comma-separated-string-but-ignoring-commas-in-quotes
+                 Dies wird geprüft, da es CSV-Dateien gibt, bei denen innerhalb von Anführungszeichen " " ein Komma (";") ist.
+                 Dies kann zu Problemen führen, da ; der Delimiter ist. Für unseren gegebenen Datensatz ist dies aber keine Limitation */
                 String[] tokens = line.split(";(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
                 if(i==0){
                     attributes = tokens;
@@ -249,7 +256,7 @@ public class Main {
                 i++;
             }
         } catch (Exception e){
-            System.out.println(e);
+            System.out.println("loading dataset failed because of: "+e);
         }
         return data;
     }
